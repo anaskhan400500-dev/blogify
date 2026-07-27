109537759b47b7f835e7cefa95e3d45dde6df40a
Anas Khan <anaskhan400500@gmail.com>
2026-07-27 20:20:22 +0530

    Add default PORT

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const comment = require("./model/comments");
 const { checkTokenAtEveryReq } = require("./middleware/authentication");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.MONGO_URL)
